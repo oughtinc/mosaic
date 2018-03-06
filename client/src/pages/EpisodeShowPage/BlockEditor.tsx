c0a26ca5cbe31029a7525639e0bb635790df8fba
Ozzie Gooen <Ozzie@grandroundshealth.com>
2018-03-06 12:32:24 -0800

    Added simple importing pointer ui

--- client/src/pages/EpisodeShowPage/BlockEditor.tsx
+++ client/src/pages/EpisodeShowPage/BlockEditor.tsx
@@ -17,13 +17,13 @@ const FormStyle = styled.div`
 const SideThing = styled.span`
     float: right;
     padding: 2px 5px;
-    background-color: #a5f8ff;
+    background-color: #f88;
     position: relative;
     display: inline-block;
     &:before {
         content: "";
         position: absolute;
-        border-right: 10px solid #a5f8ff;
+        border-right: 10px solid #f88;
         border-top: 10px solid transparent;
         border-bottom: 10px solid transparent;
         top: 2px;
@@ -31,9 +31,8 @@ const SideThing = styled.span`
     }
 `;
 
-export class PointerMark extends React.Component<any, any> {
+export class PointerExport extends React.Component<any, any> {
     public render() {
-        console.log(this.props.mark);
         return (
             <span>
                 <span style={{backgroundColor: "#f88"}}>
@@ -45,27 +44,76 @@ export class PointerMark extends React.Component<any, any> {
     }
 }
 
-export class BlockEditor extends React.Component<any, any> {
-    public updateValue(event: any, value: any) {
-        event.preventDefault();
+const ClosedPointerImport = styled.span`
+    background-color: rgba(86, 214, 252, 0.66);
+    padding: 0px 7px;
+    border-radius: 2px;
+    font-weight: 800;
+    color: #0a6e7f;
+    transition: background-color color 0.8s; 
+    &:hover {
+        transition: background-color color 0.8s; 
+        cursor: pointer;
+        background-color: rgb(112, 183, 201);
+        color: #044550;
+    }
+`;
+
+const OpenPointerImport = styled.span`
+    background-color: rgba(158, 224, 244, 0.66);
+    padding: 0px 5px;
+    border-radius: 2px;
+    font-weight: 500;
+    transition: background-color color 0.8s; 
+    &:hover {
+        cursor: pointer;
+    }
+`;
+
+export class PointerImport extends React.Component<any, any> {
+    public constructor(props: any) {
+        super(props);
+        this.state = {isOpen: false};
+    }
+    public render() {
+        if (!this.state.isOpen) {
+            return (
+                    <ClosedPointerImport onClick={() => {this.setState({isOpen: !this.state.isOpen}); }}> 
+                        $1
+                    </ClosedPointerImport>
+            );
+        } else {
+            return (
+                <OpenPointerImport onClick={() => {this.setState({isOpen: !this.state.isOpen}); }}> 
+                    This is a very funny sentence that will go right here...
+                </OpenPointerImport>
+            );
+        }
+    }
+}
+
+export class BlockEditor extends React.Component<any,  any> {
+    public updateValue(event:  any, value: any) {
+        event.preventDefault(); 
         const uuid = uuidv1();
-        const change = value.change().toggleMark({type: "pointer", object: "mark", data: {pointerId: uuid} });
-        console.log(change);
+        const change = value.change().toggleMark({type:  "pointerExport", object: "mark", data: {pointerId:  uuid} });
         return change.value;
     }
 
     public renderMark(props) {
-        const { children, mark } = props;
+        const {children,  mark } = props;
         switch (mark.type) {
-          case "pointer":
-            return <PointerMark mark={mark.toJSON()}>{children}</PointerMark>;
+          case "pointerExport":
+            return <PointerExport mark={mark.toJSON()}>{children}</PointerExport>;
+          case "pointerImport":
+            return <PointerImport mark={mark.toJSON()}>{children}</PointerImport>;
           default: 
             return {children};
         }
       }
 
     public render() {
-        if (this.props.isInField) {
+        if  (this.props.isInField) {
             return (
                 <Field
                     name={this.props.name}
@@ -77,6 +125,17 @@ export class BlockEditor extends React.Component<any, any> {
                              onClick={(e) => input.onChange(this.updateValue(e, input.value))}
                             >
                                 CHANGE
+                            </Button>
+                            <Button
+                             onClick={(event) => {
+                                 const ch = input.value.change()
+                                    .insertText("hi there!")
+                                    .extend(0 - "hi there!".length)
+                                    .addMark({type: "pointerImport", object: "mark", data: {pointerId: "sd8fjsdf8js"}});
+                                 input.onChange(ch.value);
+                             }}
+                            >
+                               Add 
                             </Button>
                             <Editor
                                 value={input.value}
@@ -92,6 +151,7 @@ export class BlockEditor extends React.Component<any, any> {
             return (
                 <Editor
                     value={this.props.value || Plain.deserialize("")}
+                    renderMark={this.renderMark}
                     readOnly={true}
                 />
             );
