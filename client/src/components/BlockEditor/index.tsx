323958d0f7a730be31a27c7754c0b4b4adfdb03f
Andreas Stuhlmueller <andreas@stuhlmueller.org>
2018-03-07 14:39:20 -0800

    Add border, margin, padding to BlockEditor

--- client/src/components/BlockEditor/index.tsx
+++ client/src/components/BlockEditor/index.tsx
@@ -1,118 +1,127 @@
 import * as React from "react";
-import Plain from "slate-plain-serializer";
-import { Editor } from "slate-react";
-import _ = require("lodash");
-import { Button, ButtonGroup, DropdownButton, MenuItem } from "react-bootstrap";
+import * as ReactDOM from "react-dom";
+import * as _ from "lodash";
 import * as uuidv1 from "uuid/v1";
-import ReactDOM = require("react-dom");
+import Plain from "slate-plain-serializer";
+import styled from "styled-components";
+import { Button, ButtonGroup, DropdownButton, MenuItem } from "react-bootstrap";
+import { Editor } from "slate-react";
 import { PointerExportMark } from "./PointerExportMark";
 import { PointerImportMark } from "./PointerImportMark";
-import { compose } from "recompose";
 import { addBlocks, updateBlock } from "../../modules/blocks/actions";
-import { connect } from "react-redux";
 import { changePointerReference, changeHoverItem } from "../../modules/blockEditor/actions";
+import { compose } from "recompose";
+import { connect } from "react-redux";
+
+const BlockEditorStyle = styled.div`
+    border: 1px solid #ddd;
+    margin-bottom: 1em;
+    padding: .5em;
+`
 
 class BlockEditorPresentational extends React.Component<any, any> {
-    public menu;
-    public constructor(props: any) {
-        super(props);
-        this.renderMark = this.renderMark.bind(this);
-        this.onSelect = this.onSelect.bind(this);
+  public menu;
+  public constructor(props: any) {
+    super(props);
+    this.renderMark = this.renderMark.bind(this);
+    this.onSelect = this.onSelect.bind(this);
+  }
+
+  public onSelect(event: any) {
+    const block = this.props.blocks.blocks.find((b) => b.id === this.props.blockId);
+    const value = block.value;
+    if (value.isBlurred || value.isEmpty) {
+      return;
     }
 
-    public onSelect(event: any) {
-        const block = this.props.blocks.blocks.find((b) => b.id === this.props.blockId);
-        const value = block.value;
-        if (value.isBlurred || value.isEmpty) {
-            return;
-        }
+    const selection = window.getSelection();
+    const range = selection.getRangeAt(0);
+    const rect = range.getBoundingClientRect();
 
-        const selection = window.getSelection();
-        const range = selection.getRangeAt(0);
-        const rect = range.getBoundingClientRect();
-
-        if (rect.width === 0) {
-            window.setTimeout(
-                () => {
-                this.props.changeHoverItem({ hoverItemType: "NONE", id: false, top: false, left: false, blockId: false  });
-                }, 10);
-            return;
-        }
-
-        const _top = `${(rect.top - 44).toFixed(2)}px`;
-        const _left = `${(rect.left.toFixed(2))}px`;
-        const { hoveredItem: { id, top, left } } = this.props.blockEditor;
-        if ((_top !== top) || (_left !== left)) {
-            window.setTimeout(
-                () => {
-                    this.props.changeHoverItem({ hoverItemType: "SELECTED_TEXT", id: "NOT_NEEDED", top: _top, left: _left, blockId: this.props.blockId  });
-                }, 10
-            );
-        }
+    if (rect.width === 0) {
+      window.setTimeout(
+        () => {
+          this.props.changeHoverItem({ hoverItemType: "NONE", id: false, top: false, left: false, blockId: false  });
+        }, 10);
+      return;
     }
 
-    public renderMark(props) {
-        const { children, mark } = props;
-        switch (mark.type) {
-            case "pointerExport":
-                return <PointerExportMark mark={mark.toJSON()}>{children}</PointerExportMark>;
-            case "pointerImport":
-                const { internalReferenceId } = mark.toJSON().data;
-                const reference = this.props.blockEditor.pointerReferences[internalReferenceId];
-                return (
-                    <PointerImportMark
-                        mark={mark.toJSON()}
-                    >
-                        {children}
-                    </PointerImportMark>
-                );
-            default:
-                return { children };
-        }
+    const _top = `${(rect.top - 44).toFixed(2)}px`;
+    const _left = `${(rect.left.toFixed(2))}px`;
+    const { hoveredItem: { id, top, left } } = this.props.blockEditor;
+    if ((_top !== top) || (_left !== left)) {
+      window.setTimeout(
+        () => {
+          this.props.changeHoverItem({ hoverItemType: "SELECTED_TEXT", id: "NOT_NEEDED", top: _top, left: _left, blockId: this.props.blockId  });
+        }, 10
+      );
     }
+  }
 
-    public render() {
-        const { readOnly } = this.props;
-        const block = this.props.blocks.blocks.find((b) => b.id === this.props.blockId);
-        const value = block.value;
-        const onChange = (value) => { this.props.updateBlock({ id: block.id, value }); };
-        const exportingLeaves = _.flatten(this.props.blocks.blocks.map((b) => b.exportingLeaves));
-        if (readOnly) {
-            return (
-                <Editor
-                    value={this.props.value || Plain.deserialize("")}
-                    renderMark={this.renderMark}
-                    readOnly={true}
-                />
-            );
-        } else {
-            return (
-                <div>
-                    <DropdownButton title="Import" id="bg-nested-dropdown">
-                        {exportingLeaves.map((e: any) => (
-                            <MenuItem
-                                eventKey="1"
-                                onClick={(event) => {
-                                    const ch = value.change()
-                                        .insertText("~")
-                                        .extend(0 - "~".length)
-                                        .addMark({ type: "pointerImport", object: "mark", data: { pointerId: e.pointerId, internalReferenceId: uuidv1() } });
-                                    onChange(ch.value);
-                                }}
-                            >
-                                {e.pointerId}
-                            </MenuItem>
-                        ))}
-                    </DropdownButton>
-                    <Editor
-                        value={value}
-                        onChange={(c) => { onChange(c.value); }}
-                        renderMark={this.renderMark}
-                        onSelect={this.onSelect}
-                    />
+  public renderMark(props) {
+    const { children, mark } = props;
+    switch (mark.type) {
+      case "pointerExport":
+        return <PointerExportMark mark={mark.toJSON()}>{children}</PointerExportMark>;
+      case "pointerImport":
+        const { internalReferenceId } = mark.toJSON().data;
+        const reference = this.props.blockEditor.pointerReferences[internalReferenceId];
+        return (
+          <PointerImportMark
+            mark={mark.toJSON()}
+          >
+            {children}
+          </PointerImportMark>
+        );
+      default:
+        return { children };
+    }
+  }
 
-                </div>
-            );
+  public render() {
+    const { readOnly } = this.props;
+    const block = this.props.blocks.blocks.find((b) => b.id === this.props.blockId);
+    const value = block.value;
+    const onChange = (value) => { this.props.updateBlock({ id: block.id, value }); };
+    const exportingLeaves = _.flatten(this.props.blocks.blocks.map((b) => b.exportingLeaves));
+    if (readOnly) {
+      return (
+        <Editor
+          value={this.props.value || Plain.deserialize("")}
+          renderMark={this.renderMark}
+          readOnly={true}
+        />
+      );
+    } else {
+      return (
+        <div>
+          <DropdownButton title="Import" id="bg-nested-dropdown">
+            {exportingLeaves.map((e: any) => (
+              <MenuItem
+                eventKey="1"
+                onClick={(event) => {
+                    const ch = value.change()
+                                    .insertText("~")
+                                    .extend(0 - "~".length)
+                                    .addMark({ type: "pointerImport", object: "mark", data: { pointerId: e.pointerId, internalReferenceId: uuidv1() } });
+                    onChange(ch.value);
+                }}
+                >
+                {e.pointerId}
+              </MenuItem>
+            ))}
+          </DropdownButton>
+          <BlockEditorStyle>
+            <Editor
+              value={value}
+              onChange={(c) => { onChange(c.value); }}
+              renderMark={this.renderMark}
+              onSelect={this.onSelect}
+            />
+          </BlockEditorStyle>
+
+        </div>
+      );
         }
     }
 }
