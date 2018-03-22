f743e2f156349d33f9abce3fe7a883257cd68754
Ozzie Gooen <Ozzie@grandroundshealth.com>
2018-03-21 17:42:53 -0700

    Replaced use of binds() in components to = () => syntax

--- client/src/pages/EpisodeShowPage/index.tsx
+++ client/src/pages/EpisodeShowPage/index.tsx
@@ -86,10 +86,9 @@ const ParentLink = (props) => (
 export class FormPagePresentational extends React.Component<any, any> {
     public constructor(props: any) {
         super(props);
-        this.updateBlocks = this.updateBlocks.bind(this);
     }
 
-    public updateBlocks(blocks: any) {
+    public updateBlocks = (blocks: any) => {
         const variables = { blocks };
         this.props.updateBlocks({
             variables,
