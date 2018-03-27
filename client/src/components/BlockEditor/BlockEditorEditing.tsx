b9fc009276c203b5753c335d5b5ecf8ad12f66a4
Ozzie Gooen <Ozzie@grandroundshealth.com>
2018-03-26 17:51:02 -0700

    Control-E keyboard shortcut added for adding pointers

--- client/src/components/BlockEditor/BlockEditorEditing.tsx
+++ client/src/components/BlockEditor/BlockEditorEditing.tsx
@@ -10,6 +10,7 @@ import { connect } from "react-redux";
 import { updateBlock } from "../../modules/blocks/actions";
 import { MenuBar } from "./MenuBar";
 import { MutationStatus } from "./types";
+import { exportSelection } from "../../modules/blockEditor/actions";
 
 const BlockEditorStyle = styled.div`
     background: #f4f4f4;
@@ -43,7 +44,8 @@ interface BlockEditorEditingPresentationalProps {
     updateBlock(value: any): () => {};
     onChange(value: any): () => boolean;
     saveBlocksMutation(): () => {};
-    onKeyDown(): () => {};
+    exportSelection(): () => {};
+    onKeyDown(event: any): () => {};
 }
 
 interface BlockEditorEditingPresentationalState {
@@ -88,10 +90,21 @@ export class BlockEditorEditingPresentational extends React.Component<BlockEdito
                     plugins={this.props.plugins}
                     spellCheck={false}
                     onBlur={this.handleBlur}
-                    onKeyDown={this.props.onKeyDown}
+                    onKeyDown={this.onKeyDown}
                 />
             </BlockEditorStyle>
         );
+    }
+
+    private onKeyDown = (event) => {
+        const controlE = (_event) => (_event.ctrlKey && _event.key === "e");
+        if (controlE(event)) {
+            this.props.exportSelection();
+            event.preventDefault();
+        }
+        if (!!this.props.onKeyDown) {
+            this.props.onKeyDown(event);
+        }
     }
 
     private onValueChange = () => {
@@ -167,7 +180,7 @@ function mapStateToProps(state: any) {
 
 export const BlockEditorEditing: any = compose(
     connect(
-        mapStateToProps, { updateBlock }
+        mapStateToProps, { updateBlock, exportSelection }
     ),
     graphql(UPDATE_BLOCKS, { name: "saveBlocksToServer" }),
     withState("mutationStatus", "setMutationStatus", { status: MutationStatus.NotStarted }),
