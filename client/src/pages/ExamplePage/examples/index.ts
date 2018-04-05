291e44a1251f10c289a47cbf420f04242be1bf89
Ozzie Gooen <Ozzie@grandroundshealth.com>
2018-04-04 22:53:55 -0700

    Added simple sample example

---
+++ client/src/pages/ExamplePage/examples/index.ts
@@ -0,0 +1,10 @@
+import { data as sampleData } from "./sample";
+
+export const examples = [
+    {
+        url: "sample",
+        name: "Sample",
+        rootWorkspaceId: "77e83b98-c228-43c2-9b96-16c48dbd1a85",
+        data: sampleData,
+    },
+];
---
+++ client/src/pages/ExamplePage/index.tsx
@@ -0,0 +1,34 @@
+import gql from "graphql-tag";
+import { compose } from "recompose";
+import { graphql } from "react-apollo";
+import { BlockHoverMenu } from "../../components/BlockHoverMenu";
+import * as React from "react";
+import _ = require("lodash");
+import { WorkspaceCard } from "../../components/WorkspaceCard";
+import { examples } from "./examples";
+
+export class ExamplePage extends React.Component<any, any> {
+    public render() {
+        const {exampleName} = this.props.match.params;
+        const example: any = examples.find((e:any) => e.url === exampleName);
+        const workspaces = example.data.subtreeWorkspaces;
+        const availablePointers: any = _
+          .chain(workspaces)
+          .map((w: any) => w.connectedPointers)
+          .flatten()
+          .uniqBy((p: any) => p.data.pointerId)
+          .value();
+        const rootWorkspace = workspaces.find((w) => w.id === example.rootWorkspaceId);
+        return (
+            <div>
+                <h2> {example.name} </h2>
+                <hr/>
+                <BlockHoverMenu>
+                    {rootWorkspace &&
+                        <WorkspaceCard workspace={rootWorkspace} availablePointers={availablePointers} workspaces={workspaces} />
+                    }
+                </BlockHoverMenu>
+            </div>
+        );
+    }
+}
