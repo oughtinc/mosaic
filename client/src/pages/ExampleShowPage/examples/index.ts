ebc9d952753ffb2b8ffd9e301e6606f01104d65b
Ozzie Gooen <Ozzie@grandroundshealth.com>
2018-04-04 23:09:37 -0700

    Changed ExamplePage to ExampleShowPage

--- client/src/pages/ExampleShowPage/examples/index.ts
+++ client/src/pages/ExampleShowPage/examples/index.ts
@@ -1,5 +1,8 @@
 import { data as sampleData } from "./sample";
 
+// It's suggested to use this tool to transform graphql responses into examples:
+// https://json-to-js.com/
+
 export const examples = [
     {
         url: "sample",
