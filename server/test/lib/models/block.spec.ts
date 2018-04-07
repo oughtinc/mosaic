36d692a686eb89f9b31f520041f9b97a3c88420c
Ben Rachbach <brachbach@gmail.com>
2018-04-06 20:15:14 -0700

    Add launch.json to be able to run w/ debugging in vscode

--- server/test/lib/models/block.spec.ts
+++ server/test/lib/models/block.spec.ts
@@ -26,7 +26,6 @@ const mockTree = {
 describe('getAllInlinesAsArray', () => {
     it('should get all inline nodes', () => {
         const inlines = getAllInlinesAsArray(mockTree);
-        console.log(JSON.stringify(inlines));
         expect(inlines).to.include(outerInlineNode);
         expect(inlines).to.include(innerInlineNode);
     })
