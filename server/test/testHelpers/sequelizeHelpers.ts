2944b1c1789a4bb9a09e0bc222306f11400576d4
Ben Rachbach <brachbach@gmail.com>
2018-04-10 10:54:59 -0700

    Actually test stuff in the integration test, and modify the truncate function to just exclude the events table

--- server/test/testHelpers/sequelizeHelpers.ts
+++ server/test/testHelpers/sequelizeHelpers.ts
@@ -1,15 +1,14 @@
 import * as models from "../../lib/models";
 
+// sequelize is in here b/c it's not actually one of our tables.
+// Event is in here b/c clearing the events table
+// creates all sorts of weird foreign key problems, and isn't necessary,
+// at least for our current tests.
+const keysToExclude = ['sequelize', 'Sequelize', 'Event'];
+
 export const truncate = () => Promise.all(Object.keys(models).map((key) => {
-    if (['sequelize', 'Sequelize'].indexOf(key) > -1) {
+    if (keysToExclude.indexOf(key) > -1) {
         return;
     }
     return models[key].destroy({ where: {}, force: true });
 }))
-    .catch(truncate);
-// this recursive catch is here b/c
-// sometimes some of the tables don't get truncated on the first pass
-// b/c of foreign key constraints.
-// I've only ever seen two passes needed,
-// and of course this is a test helper that's not used outside of tests,
-// so the slight hackiness seems fine to me.
