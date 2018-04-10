aaf556a75e506d14a202f623d413a2b43ca8c00c
Ben Rachbach <brachbach@gmail.com>
2018-04-10 10:54:59 -0700

    Actually test stuff in the integration test, and modify the truncate function to just exclude the events table

--- server/test/integrationTests.spec.ts
+++ server/test/integrationTests.spec.ts
@@ -22,9 +22,16 @@ describe('createWorkspace for a root workspace', () => {
     beforeEach(() => {
         truncate();
     })
-    
-    it('creates a root workspace', () => {
-        return createRootWorkspace();
+
+    it('creates a root workspace', async () => {
+        const rootWorkspace = await createRootWorkspace();
+        const workspaceInfo = rootWorkspace.data.createWorkspace;
+        console.log(JSON.stringify(workspaceInfo.blocks));
+        expect(workspaceInfo.totalBudget === 1000);
+
+        // this is kind of a lame test,
+        // but I didn't really want to add more specificity that would make this more brittle
+        expect(workspaceInfo.blocks).to.not.be.empty;
     })
 
 })
