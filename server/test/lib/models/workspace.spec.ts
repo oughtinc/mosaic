254383c89b33e74dffe5c5ad8f80bf9732ba9fdc
Ben Rachbach <brachbach@gmail.com>
2018-04-11 15:35:28 -0700

    Use only sequelize (not GraphQL) to test sequelize, per PR comments

--- server/test/lib/models/workspace.spec.ts
+++ server/test/lib/models/workspace.spec.ts
@@ -1,21 +1,24 @@
 import * as chai from "chai";
 const { expect } = chai;
 import { truncateDb } from "../../testHelpers/sequelizeHelpers";
-import { createRootWorkspace } from "../../testHelpers/graphQLCalls";
 import WorkspaceModel from "../../../lib/models/workspace";
 import * as models from "../../../lib/models";
 
 describe('WorkspaceModel', () => {
     describe('subtreeWorkspaces', () => {
-        beforeEach(() => truncateDb());
         
         // a lame test, but just wanted to get something working quickly
         // to show the testing pattern.
-        it('should not return anything if there are no subtree workspaces', async () => {
-            const rootWorkspace = await createRootWorkspace();
-            const workspace = await models.Workspace.findById(rootWorkspace.data.createWorkspace.id);
-            const subtreeWorkspaces = await workspace.subtreeWorkspaces();
-            expect(subtreeWorkspaces).to.be.empty;
-        });
+        context('when there are no subtree workspaces', () => {
+            beforeEach(() => truncateDb());
+            it('should not return anything', async () => {
+                const totalBudget = 1000;
+                const question = "{\"object\":\"block\",\"type\":\"line\",\"isVoid\":false,\"data\":{},\"nodes\":[{\"object\":\"text\",\"leaves\":[{\"object\":\"leaf\",\"text\":\"Fake root workspace\",\"marks\":[]}]}]}";
+                const event = await models.Event.create();
+                const workspace = await models.Workspace.create({ totalBudget }, { event, questionValue: JSON.parse(question) });
+                const subtreeWorkspaces = await workspace.subtreeWorkspaces();
+                expect(subtreeWorkspaces).to.be.empty;
+            });
+        })
     });
 })
