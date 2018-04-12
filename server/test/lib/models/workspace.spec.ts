7e44fe3b87cc5953a41706ef023201678ac430d4
Ben Rachbach <brachbach@gmail.com>
2018-04-11 18:28:42 -0700

    Improvements per Ozzie PR review round 2

--- server/test/lib/models/workspace.spec.ts
+++ server/test/lib/models/workspace.spec.ts
@@ -1,6 +1,7 @@
 import * as chai from "chai";
 const { expect } = chai;
 import { truncateDb } from "../../testHelpers/sequelizeHelpers";
+import { rootWorkspaceQuestion } from "../../testHelpers/slateHelpers";
 import WorkspaceModel from "../../../lib/models/workspace";
 import * as models from "../../../lib/models";
 
@@ -9,11 +10,11 @@ describe('WorkspaceModel', () => {
         
         // a lame test, but just wanted to get something working quickly
         // to show the testing pattern.
-        context('when there are no subtree workspaces', () => {
+        context('a workspace with no child workspaces', () => {
             beforeEach(() => truncateDb());
             it('should not return anything', async () => {
                 const totalBudget = 1000;
-                const question = "{\"object\":\"block\",\"type\":\"line\",\"isVoid\":false,\"data\":{},\"nodes\":[{\"object\":\"text\",\"leaves\":[{\"object\":\"leaf\",\"text\":\"Fake root workspace\",\"marks\":[]}]}]}";
+                const question = rootWorkspaceQuestion('Mock workspace name');
                 const event = await models.Event.create();
                 const workspace = await models.Workspace.create({ totalBudget }, { event, questionValue: JSON.parse(question) });
                 const subtreeWorkspaces = await workspace.subtreeWorkspaces();
