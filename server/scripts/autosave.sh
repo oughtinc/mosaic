1d7b2eafe0417a7434d23c097105b6d412ca9f7d
Ben Rachbach <brachbach@gmail.com>
2018-04-18 15:03:03 -0700

    Autosave only if the db content differs from what it was on the last autosave

--- server/scripts/autosave.sh
+++ server/scripts/autosave.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
-save () {
-    scripts/dumpDB.sh autosave`date +%s`
-    echo "Autosave complete!"
-    sleep 5
-    save
+
+autosave () {
+    previous_autosave_path=$(ls -1 dbDumps/autosave* | tail -n 1)
+    
+    # create current autosave
+    current_autosave_name=autosave`date +%s`
+    scripts/dumpDB.sh $current_autosave_name
+
+    # if contents of the current autosave are the same as contents of the previous autosave, delete the current autosave
+    if [ $previous_autosave_path ]
+        then
+            current_autosave_path=dbDumps/${current_autosave_name}.db
+            if cmp --silent "$previous_autosave_path" "$current_autosave_path"
+                then
+                    rm $current_autosave_path
+            fi
+    fi
+
+    sleep $1
+    autosave $1
 }
 
-save
+if [ $# -eq 0 ]
+    then
+        echo "ERROR: Please specify how many seconds to sleep for."
+        exit 1
+fi
+
+autosave $1
+
+
