8f49ab52effd06243d91301e6e159571797dd70b
Ben Rachbach <brachbach@gmail.com>
2018-04-18 16:38:30 -0700

    Attempting to get autodump to kick off as part of `docker compose up`. Having a lot of trouble getting this to work.

---
+++ server/scripts/autodump.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Note: this script is run automatically by `docker compose-up`
+# You can find the autodumps in server/autodumps,
+# and you can copy one to server/dbDumps in order to restore ir
+
+autodump () {
+    previous_autodump_path=$(ls autodumps/autodump* | tail -n 1)
+    
+    # create current autodump
+    current_autodump_name=autodump`date +%s`
+    [ -d autodumps ] || mkdir autodumps
+    pg_dump --host=localhost --username=mosaic mosaic_dev > autodumps/$current_autodump_name.db
+
+    # if contents of the current autodump are the same as contents of the previous autodump, delete the current autodump
+    if [ $previous_autodump_path ]
+        then
+            current_autodump_path=autodumps/${current_autodump_name}.db
+            if cmp --silent "$previous_autodump_path" "$current_autodump_path"
+                then
+                    rm $current_autodump_path
+            fi
+    fi
+
+    sleep $1
+    autodump $1
+}
+
+if [ $# -eq 0 ]
+    then
+        echo "ERROR: Please specify how many seconds to sleep for."
+        exit 1
+fi
+
+autodump $1
+
+
