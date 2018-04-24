5693b75b3b142bfac70359194e2b5c58b97e542b
Ben Rachbach <brachbach@gmail.com>
2018-04-24 12:17:33 -0700

    Have user specify path for dump and restore (Fixes #73). Note that this changes the dump/restore API

--- server/scripts/dumpDB.sh
+++ server/scripts/dumpDB.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 if [ $# -eq 0 ]
   then
-    echo "ERROR: Please supply a name for this dump."
+    echo "ERROR: Please supply a filepath for this dump."
     exit 1
 fi
-[ -d dbDumps ] || mkdir dbDumps
-docker exec mosaic_postgres_1 pg_dump --host=localhost --username=mosaic mosaic_dev > dbDumps/$1.db
+
+dump_dir=$(dirname $1)
+
+if [ ! -d $dump_dir ]; then
+    echo "ERROR: There is no directory at the filepath you specified."
+    exit 1
+fi
+
+docker exec mosaic_postgres_1 pg_dump --host=localhost --username=mosaic mosaic_dev > $1
