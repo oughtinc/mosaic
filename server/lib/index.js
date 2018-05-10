0d0c74df94c4fa08f451a4867ae605e57b9447e1
andrewschreiber <andrew.schreiber1@gmail.com>
2018-05-09 17:33:53 -0700

    style: cleanup and logging

--- server/lib/index.js
+++ server/lib/index.js
@@ -1,12 +1,12 @@
 import 'babel-polyfill';
 
 import * as express from 'express';
+import * as path from 'path';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import * as bodyParser from 'body-parser';
-const path = require('path');
+var cors = require('cors');
 
 import { schema } from './schema/index';
-var cors = require('cors');
 
 const GRAPHQL_PORT = process.env.PORT || 8080;
 
@@ -25,5 +25,5 @@ graphQLServer.get('*', (req, res) => {
 
 
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
-  `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
+  `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql. \nReact is now running on http://localhost:${GRAPHQL_PORT}`
 ));
