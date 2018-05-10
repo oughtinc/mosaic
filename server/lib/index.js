4bfbb743c9bd38930ef60213ea25569a05b4f5a4
andrewschreiber <andrew.schreiber1@gmail.com>
2018-05-09 17:30:33 -0700

    feat: locally serving react via express

--- server/lib/index.js
+++ server/lib/index.js
@@ -3,16 +3,26 @@ import 'babel-polyfill';
 import * as express from 'express';
 import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 import * as bodyParser from 'body-parser';
-import {schema} from './schema/index';
+const path = require('path');
+
+import { schema } from './schema/index';
 var cors = require('cors');
 
 const GRAPHQL_PORT = process.env.PORT || 8080;
 
 const graphQLServer = express();
+
+graphQLServer.use(express.static(path.join(__dirname, '/../../client/build')));
 graphQLServer.use(cors())
 
 graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
 graphQLServer.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+
+graphQLServer.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/../../client/build/public/index.html'));
+});
+
+
 
 graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphiQL is now running on http://localhost:${GRAPHQL_PORT}/graphiql`
