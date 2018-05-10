6ed4320d2069da6b929e0d8f12807d44c545fe88
andrewschreiber <andrew.schreiber1@gmail.com>
2018-05-10 01:03:09 -0700

    fix: proper null coalesce

--- server/lib/models/index.js
+++ server/lib/models/index.js
@@ -13,7 +13,7 @@ const PointerImportModel = require('./pointerImport');
 const WorkspaceModel = require('./workspace');
 
 if (config.use_env_variable) {
-  const dbURL = process.env["DATABASE_URL"] ?? process.env[config.use_env_variable]
+  const dbURL = process.env["DATABASE_URL"] || process.env[config.use_env_variable];
   var sequelize = new Sequelize(dbURL, config);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
