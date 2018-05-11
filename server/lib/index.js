8206e527343a1ae0cff36317369022cbe45beea7
andrewschreiber <andrew.schreiber1@gmail.com>
2018-05-10 17:35:07 -0700

    feat: move static

--- server/lib/index.js
+++ server/lib/index.js
@@ -14,20 +14,19 @@ const GRAPHQL_PORT = process.env.PORT || 8080;
 
 const graphQLServer = express();
 
-graphQLServer.use(express.static(path.join(__dirname, '/../../client/build')));
 graphQLServer.use(cors())
 
 if (!process.env.USING_DOCKER) {
   graphQLServer.use(enforce.HTTPS({ trustProtoHeader: true }));
-  graphQLServer.use((req, res, next) => {
-    if (req.header('x-forwarded-proto') !== 'https') {
-      res.redirect(`https://${req.header('host')}${req.url}`)
-    } else {
-      next()
-    }
-  });
+  // graphQLServer.use((req, res, next) => {
+  //   if (req.header('x-forwarded-proto') !== 'https') {
+  //     res.redirect(`https://${req.header('host')}${req.url}`)
+  //   } else {
+  //     next()
+  //   }
+  // });
 }
-
+graphQLServer.use(express.static(path.join(__dirname, '/../../client/build')));
 
 
 graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({ schema }));
