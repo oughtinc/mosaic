c229fe65f568b55752dda2da87742c1eea8627a3
andrewschreiber <andrew.schreiber1@gmail.com>
2018-05-31 16:29:38 -0700

    feat: add error handling

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import ApolloClient from "apollo-client";
 import { ApolloProvider } from "react-apollo";
 import { HttpLink } from "apollo-link-http";
+import { ApolloLink } from "apollo-link";
+import { onError } from "apollo-link-error";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { EpisodeShowPage } from "./pages/EpisodeShowPage";
@@ -23,8 +25,26 @@ const SERVER_URL = window.location.hostname === "localhost" ?
   "http://localhost:8080/graphql" :
   `${window.location.protocol}//${window.location.hostname}/graphql`;
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
+      )
+    );
+  }
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`);
+  }
+});
+
+const link = ApolloLink.from([
+  errorLink,
+  new HttpLink({ uri: SERVER_URL }),
+]);
+
 const client: any = new ApolloClient({
-  link: new HttpLink({ uri: SERVER_URL }),
+  link,
   cache: new InMemoryCache(),
 });
 
