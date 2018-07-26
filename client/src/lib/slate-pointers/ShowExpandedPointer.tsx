55c00b2a36b8df7ce0c724dc3cd88501371aefc3
Zachary Miller <zachary.john.miller@gmail.com>
2018-07-26 14:50:26 -0600

    Adjust expanded imported pointer styling

--- client/src/lib/slate-pointers/ShowExpandedPointer.tsx
+++ client/src/lib/slate-pointers/ShowExpandedPointer.tsx
@@ -57,8 +57,18 @@ export class ShowExpandedPointer extends React.Component<any, any> {
   }
 
   public render() {
+    const isNestedAsExportingPointer = this.props.availablePointers.some(pointer => {
+      return pointer.nodes.some(node => {
+        return (node.type === 'pointerExport' && node.data.pointerId === this.props.exportingPointer.data.pointerId);
+      })
+    });
+
     return (
-      <div>
+      <div
+        style={{
+          paddingLeft: isNestedAsExportingPointer ? '5px' : 0,
+        }}
+      >
         {this.props.exportingPointer.nodes.map((node, index) => {
           if (
             node.object === "inline" ||
