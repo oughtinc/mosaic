e9792adb55902112699c49c61f01b64848947c75
Zachary Miller <zachary.john.miller@gmail.com>
2018-08-08 16:50:13 -0600

    Account for immutability  in spacer normalization
    Changes aren't reflected in the old text nodes, because in Slate
    everything is immutable. So we need to fetch the "new" node to make
    sure we are up-to-date. Similarly, we'll use the newest document
    object instead of relying on the old (possibly out-of-date) one.

--- client/src/components/BlockEditor/normalizeChange.js
+++ client/src/components/BlockEditor/normalizeChange.js
@@ -4,40 +4,43 @@ import { Change, TextNode } from './types';
 export function normalizeExportSpacing(c: Change) {
   const value = c.value;
 
-  function directlyBeforeExport(textNode: TextNode) {
-    return value.document.getNextSibling(textNode.key) && value.document.getNextSibling(textNode.key).type === "pointerExport";
+  function directlyBeforeExport(textNode: TextNode, document) {
+    return document.getNextSibling(textNode.key) && value.document.getNextSibling(textNode.key).type === "pointerExport";
   }
 
-  function directlyAfterExport(textNode: TextNode) {
-    return value.document.getPreviousSibling(textNode.key) && value.document.getPreviousSibling(textNode.key).type === "pointerExport";
+  function directlyAfterExport(textNode: TextNode, document) {
+    return document.getPreviousSibling(textNode.key) && value.document.getPreviousSibling(textNode.key).type === "pointerExport";
   }
 
-  function insideExport(textNode: TextNode) {
-    return value.document.getParent(textNode.key) && value.document.getParent(textNode.key).type === "pointerExport";
+  function insideExport(textNode: TextNode, document) {
+    return document.getParent(textNode.key) && value.document.getParent(textNode.key).type === "pointerExport";
   }
 
-  function firstCharIsSpacer(textNode: TextNode) {
+  function firstCharIsSpacer(textNode: TextNode, document) {
     return textNode.text.charAt(0) === SPACER;
   }
 
-  function lastCharIsSpacer(textNode: TextNode) {
+  function lastCharIsSpacer(textNode: TextNode, document) {
     return textNode.text.charAt(textNode.text.length - 1) === SPACER;
   }
 
   value.document.getTexts().forEach(textNode => {
-    if (!insideExport(textNode) && directlyBeforeExport(textNode) && !lastCharIsSpacer(textNode)) {
+    if (!insideExport(textNode, c.value.document) && directlyBeforeExport(textNode, c.value.document) && !lastCharIsSpacer(textNode, c.value.document)) {
       c.insertTextByKey(textNode.key, textNode.text.length, SPACER);
     }
 
-    if (!insideExport(textNode) && directlyAfterExport(textNode) && !firstCharIsSpacer(textNode)) {
+    textNode = c.value.document.getNode(textNode.key);
+    if (!insideExport(textNode, c.value.document) && directlyAfterExport(textNode, c.value.document) && !firstCharIsSpacer(textNode, c.value.document)) {
       c.insertTextByKey(textNode.key, 0, SPACER);
     }
 
-    if (insideExport(textNode) && !firstCharIsSpacer(textNode)) {
+    textNode = c.value.document.getNode(textNode.key);
+    if (insideExport(textNode, c.value.document) && !firstCharIsSpacer(textNode, c.value.document)) {
       c.insertTextByKey(textNode.key, 0, SPACER);
     }
 
-    if (insideExport(textNode) && !lastCharIsSpacer(textNode)) {
+    textNode = c.value.document.getNode(textNode.key);
+    if (insideExport(textNode, c.value.document) && !lastCharIsSpacer(textNode, c.value.document)) {
       c.insertTextByKey(textNode.key, textNode.text.length, SPACER);
     }
 
@@ -45,7 +48,8 @@ export function normalizeExportSpacing(c: Change) {
     // and you delete one, then first and last char are both spaces
     // since they are the same char, so you need to additionaly enforce
     // that there are at least two chars
-    if (insideExport(textNode) && textNode.text.length === 1) {
+    textNode = c.value.document.getNode(textNode.key);
+    if (insideExport(textNode, c.value.document) && textNode.text.length === 1) {
       c.insertTextByKey(textNode.key, 0, SPACER);
     }
   });
