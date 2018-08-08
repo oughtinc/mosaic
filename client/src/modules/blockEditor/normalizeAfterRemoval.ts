f262b218cb9ff75d18286da58d110af8a8ad03ea
Zachary Miller <zachary.john.miller@gmail.com>
2018-08-08 17:00:38 -0600

    After removal only normalize text that has spacers
    This will make the process more efficient.

--- client/src/modules/blockEditor/normalizeAfterRemoval.ts
+++ client/src/modules/blockEditor/normalizeAfterRemoval.ts
@@ -9,8 +9,11 @@ export function normalizeAfterRemoval(c: Change) {
     // remove all spacer characters
     const oldText = textNode.text;
     const newText = oldText.split(SPACER).join("");
-    c.insertTextByKey(textNode.key, textNode.text.length, newText);
-    c.removeTextByKey(textNode.key, 0, textNode.text.length);
+
+    if (oldText !== newText) {
+      c.insertTextByKey(textNode.key, textNode.text.length, newText);
+      c.removeTextByKey(textNode.key, 0, textNode.text.length);
+    }
 
     // add required space characters
     normalizeExportSpacing(c);
