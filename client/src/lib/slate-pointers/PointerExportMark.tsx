7cacb4fa33b5a5f44de0456777a969bbc57abdcc
Zachary Miller <zachary.john.miller@gmail.com>
2018-08-14 16:44:10 -0600

    Use aphrodite to style exported pointer
    This is instead of styled-components, which was causing problems when
    imported pointers were nested inside exported pointers. In particular,
    prior to this commit, if pointer A was nested inside pointer B, and you
    imported A inside exported pointer C, then you could expand A within C,
    but you couldn't expand B. This bug doesn't arise with Aphrodite
    though. In this component we determine the css on a case-by-case basis
    inside the render method, so it's no surprise that not all React style
    libraries can handle all aspects of this case smoothly.

--- client/src/lib/slate-pointers/PointerExportMark.tsx
+++ client/src/lib/slate-pointers/PointerExportMark.tsx
@@ -1,3 +1,4 @@
+import { css, StyleSheet } from "aphrodite";
 import styled from "styled-components";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
@@ -9,14 +10,6 @@ import { propsToPointerDetails } from "./helpers";
 //     const possibilities = node.nodes.filter((c) => c.object === "inline");
 //     return _.max(possibilities.map((p) => (getMaxNesting(p) + 1))) || 0;
 // }
-
-const pointerExportBackground: any = ({ isSelected }: any) => {
-  if (isSelected) {
-    return "rgba(85, 228, 38, 0.9)";
-  } else {
-    return "rgba(200, 243, 197, 0.5)";
-  }
-};
 
 export class PointerExportMark extends React.Component<any, any> {
   public constructor(props: any) {
@@ -45,64 +38,71 @@ export class PointerExportMark extends React.Component<any, any> {
       children,
     }: any = this.props;
 
+    const pointerExportBackground: any =
+      isSelected
+      ?
+      "rgba(85, 228, 38, 0.9)"
+      :
+      "rgba(200, 243, 197, 0.5)";
+
     const { pointerIndex }: any = propsToPointerDetails({
       blockEditor,
       availablePointers,
       nodeAsJson
     });
 
-    const OuterPointerExportStyle: any = styled.span`
-    &::before {
-      background-color: rgba(12, 165, 0, 0.63);
-      color: rgb(233, 239, 233);
-      content: "$${parseInt(pointerIndex, 10) + 1}";
-      border-radius: 4px 0px 0px 4px;
-      padding: 0px 3px;
-    }`;
+    const styles = StyleSheet.create({
+      OuterPointerExportStyle: {
+        ':before': {
+          backgroundColor: "rgba(12, 165, 0, 0.63)",
+          color: "rgb(233, 239, 233)",
+          content: `"$${parseInt(pointerIndex, 10) + 1}"`,
+          borderRadius: "4px 0px 0px 4px",
+          padding: "0px 3px",
+        },
+      },
 
-    const PointerExportStyle: any = styled.span`
-      background: ${pointerExportBackground};
-      margin-left: 1px;
-      transition: background 0.2s;
-      color: #000000;
+      PointerExportStyle: {
+        background: `${pointerExportBackground}`,
+        color: "#000000",
+        marginLeft: "0.5px",
+        transition: "background 0.2s",
 
-      &::before {
-        color: rgba(12, 165, 0, 0.63);
-        font-size: 1.2em;
-        font-weight: 800;
-        content: "[";
-      }
+        ':before': {
+          color: "rgba(12, 165, 0, 0.63)",
+          content: `"["`,
+          font: "800 1.2em sans-serif",
+        },
 
-      &::after {
-        color: rgba(12, 165, 0, 0.63);
-        font-size: 1.2em;
-        font-weight: 800;
-        content: "]";
-      }
-    `;
+        ':after': {
+          color: "rgba(12, 165, 0, 0.63)",
+          content: `"]"`,
+          font: "800 1.2em sans-serif",
+        },
+      },
+    });
+
     return (
-      <OuterPointerExportStyle>
-        <span style={{ position: "relative" }}>
-          <PointerExportStyle
-            isSelected={isSelected}
-            onMouseOut={this.props.onMouseOut}
-          >
-              {children.map((child, index) => {
-                const isNestedPointer = child.props.node.object === "inline";
+      <span className={css(styles.OuterPointerExportStyle)}>
+        <span
+          className={css(styles.PointerExportStyle)}
+          onMouseOut={this.props.onMouseOut}
+        >
+          {children.map((child, index) => {
+            const isNestedPointer = child.props.node.object === "inline";
 
-                if (!isNestedPointer) {
-                  return (
-                    <span key={index} onMouseOver={this.onMouseOver}>
-                      {child}
-                    </span>
-                  );
-                } else {
-                  return child;
-                }
-              })}
-          </PointerExportStyle>
+            if (!isNestedPointer) {
+              return (
+                <span key={index} onMouseOver={this.onMouseOver}>
+                  {child}
+                </span>
+              );
+            } else {
+              return child;
+            }
+          })}
         </span>
-      </OuterPointerExportStyle>
+      </span>
     );
   }
 }
