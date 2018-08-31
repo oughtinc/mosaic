6014e0ddb6a9b1f39d84bf8b3331b8f41901e888
Zachary Miller <zachary.john.miller@gmail.com>
2018-08-31 16:44:53 -0600

    Add simple auth caching

--- server/lib/schema/index.ts
+++ server/lib/schema/index.ts
@@ -96,6 +96,16 @@ function userFromAuthToken(accessToken: string | null): Promise<any | null> {
     return Promise.resolve(null);
   }
 
+  // first check cache and return cached value if it's younger than 10 seconds
+  if (userFromAuthToken.cache[accessToken]) {
+    const nowTimestamp = Date.now();
+    const cacheTimestamp = userFromAuthToken.cache[accessToken].timestamp;
+    const TEN_SECONDS = 10000;
+    if (nowTimestamp - cacheTimestamp < TEN_SECONDS) {
+      return userFromAuthToken.cache[accessToken].data;
+    }
+  }
+
   return new Promise(resolve => {
     webAuth.client.userInfo(accessToken, function(err: any, user: any) {
       if (err != null) {
@@ -104,10 +114,20 @@ function userFromAuthToken(accessToken: string | null): Promise<any | null> {
       }
       const metadataKey = "https://mosaic:auth0:com/app_metadata";
       const isAdmin = user[metadataKey] ? user[metadataKey].is_admin : false;
-      return resolve({ user_id: user.sub, is_admin: isAdmin });
+      const data = { user_id: user.sub, is_admin: isAdmin };
+
+      // update cache
+      userFromAuthToken.cache[accessToken] = {
+        data,
+        timestamp: Date.now(),
+      };
+
+      return resolve(data);
     });
   });
 }
+
+userFromAuthToken.cache = {};
 
 const schema = new GraphQLSchema({
   query: new GraphQLObjectType({
