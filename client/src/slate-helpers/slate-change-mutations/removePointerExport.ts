e7009a555ce6f9df326889b1cbe202355f6b2bdb
Zachary Miller <zachary.john.miller@gmail.com>
2018-09-04 10:56:50 -0600

    Allow multi-paragraph copy-paste
    Previously, when copy-pasting multiple paragraphs, only the first
    paragraph would get saved to the server. This was because the app
    assumed each Slate document had only one block child. I generalized
    so that this assumption is no longer made. Now a Slate document might
    have a single block node as a child, or many. I also refactored
    minimally by moving the client-side getAllInlinesAsArray into its own
    file.

--- client/src/slate-helpers/slate-change-mutations/removePointerExport.ts
+++ client/src/slate-helpers/slate-change-mutations/removePointerExport.ts
@@ -1,22 +1,5 @@
+import { getInlinesAsArray } from "../slate-utils/getInlinesAsArray";
 import { Change } from "../../components/BlockEditor/types";
-
-function getInlinesAsArray(node: any) {
-  let array: any = [];
-
-  node.nodes.forEach(child => {
-    if (child.object === "text") {
-      return;
-    }
-    if (child.object === "inline") {
-      array.push(child);
-    }
-    if (!child.isLeafInline()) {
-      array = array.concat(getInlinesAsArray(child));
-    }
-  });
-
-  return array;
-}
 
 export function removePointerExport(change: Change, hoveredItem: any) {
   const value = change.value;
