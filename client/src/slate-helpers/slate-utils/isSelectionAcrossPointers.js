ce2d1f73b53e2c46ea9eed89f448ebf986f9b033
Zachary Miller <zachary.john.miller@gmail.com>
2018-09-05 11:43:54 -0600

    Shorten isSelectionAcrossPointers function

--- client/src/slate-helpers/slate-utils/isSelectionAcrossPointers.js
+++ client/src/slate-helpers/slate-utils/isSelectionAcrossPointers.js
@@ -3,15 +3,13 @@ import { containingPointerExportAncestor } from "./containingPointerExportAncest
 export function isSelectionAcrossPointers(value) {
   const { document, selection } = value;
   const { anchorKey, focusKey } = selection;
-  const anchorNode = document.getNode(anchorKey);
-  const focusNode = document.getNode(focusKey);
 
-  const containingPointerExportAncestorOfAnchorNode = containingPointerExportAncestor(anchorNode, document);
-  const containingPointerExportAncestorOfFocusNode = containingPointerExportAncestor(focusNode, document);
+  const containingPointerExportAncestorByKey =
+    key => containingPointerExportAncestor(document.getNode(key), document);
 
-  if (containingPointerExportAncestorOfAnchorNode !== containingPointerExportAncestorOfFocusNode) {
-    return true;
-  }
-
-  return false;
+  return (
+    containingPointerExportAncestorByKey(anchorKey)
+    !==
+    containingPointerExportAncestorByKey(focusKey) 
+  );
 }
