0818536927902cf59c33151019af4a3334981422
Zachary Miller <zachary.john.miller@gmail.com>
2018-09-21 08:29:06 -0600

    Tweak style of homepage

--- client/src/pages/RootWorkspacePage/index.tsx
+++ client/src/pages/RootWorkspacePage/index.tsx
@@ -35,7 +35,7 @@ const WorkspaceStyle = styled.div`
 
   border-radius: 3px;
   border-bottom: 1px solid rgba(0, 0, 0, 0.2);
-	box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);
+  box-shadow: inset 0 1px 0 rgba(255,255,255,.25), 0 1px 2px rgba(0,0,0,.05);
 `;
 
 const TextBlock = styled.div`
