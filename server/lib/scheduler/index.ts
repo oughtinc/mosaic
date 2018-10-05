f06e71df81669aa58a67a6219f758f866dbf38de
Zachary Miller <zachary.john.miller@gmail.com>
2018-10-05 14:16:46 -0600

    Add explanatory comment

--- server/lib/scheduler/index.ts
+++ server/lib/scheduler/index.ts
@@ -5,8 +5,12 @@ import { Scheduler } from "./Scheduler";
 
 const ONE_MINUTE = 60 * 1000;
 
+//  By separating out fetchAllWorkspaces and rootParentFinder, we 100% isolate
+//  the rest of the scheduling code (Scheduler, Schedule, UserSchedule, and
+//  Assignment classes) from Sequelize & Postgres
+
 const scheduler = new Scheduler({
-  fetchAllWorkspaces: () => Workspace.findAll(), // WRITE NOTE ON THIS
+  fetchAllWorkspaces: () => Workspace.findAll(),
   schedule: new Schedule({ rootParentFinder: RootParentFinder, timeLimit: ONE_MINUTE }),
   rootParentFinder: RootParentFinder,
 });
