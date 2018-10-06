f5cd4c0a0c503f411405999041b0b33bec67176a
Zachary Miller <zachary.john.miller@gmail.com>
2018-10-06 11:38:14 -0600

    Use uuidv4 for placeholder test ids

--- server/test/lib/scheduler/utils.ts
+++ server/test/lib/scheduler/utils.ts
@@ -1,7 +1,9 @@
-export const USER_ID = "user";
-export const USER_ID_1 = "user1Id";
-export const USER_ID_2 = "user2Id";
-export const WORKSPACE_ID = "workspace";
-export const WORKSPACE_ID_1 = "workspace1";
-export const WORKSPACE_ID_2 = "workspace2";
-export const WORKSPACE_ID_3 = "workspace3";
+import uuidv4 from "uuid/v4";
+
+export const USER_ID = uuidv4();
+export const USER_ID_1 = uuidv4();
+export const USER_ID_2 = uuidv4();
+export const WORKSPACE_ID = uuidv4();
+export const WORKSPACE_ID_1 = uuidv4();
+export const WORKSPACE_ID_2 = uuidv4();
+export const WORKSPACE_ID_3 = uuidv4();
