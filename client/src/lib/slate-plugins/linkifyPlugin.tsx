ae9f0bfe1cef9b5736392fd2d38901c2a475a14d
Zachary Miller <zachary.john.miller@gmail.com>
2018-10-15 14:51:56 -0600

    Add linkify plugin

---
+++ client/src/lib/slate-plugins/linkifyPlugin.tsx
@@ -0,0 +1,54 @@
+// tslint:disable-next-line
+const isUrl = require("is-url");
+import * as React from "react";
+import { getEventTransfer } from "slate-react";
+
+function wrapLink(change: any, href: any) {
+  change.wrapInline({
+    type: "link",
+    data: { href },
+  });
+}
+
+function LinkifyPlugin() {
+  return {
+    onPaste: (event, change) => {
+      const transfer = getEventTransfer(event);
+      const { type, text } = transfer;
+      if (type !== "text" && type !== "html") {
+        return;
+      }
+
+      if (!isUrl(text)) {
+        return;
+      }
+
+      change
+        .insertText(text)
+        .extend(-text.length)
+        .call(wrapLink, text);
+
+      return false;
+
+    },
+
+    renderNode(props: any) {
+      const { attributes, children, node } = props;
+
+      if (node.type === "link") {
+        const { data } = node;
+        const href = data.get("href");
+
+        return (
+          <a {...attributes} href={href}>
+            {children}
+          </a>
+        );
+      }
+
+      return;
+    }
+  };
+}
+
+export { LinkifyPlugin };
