255c79f1a02afb7dbebb50ffa2f7da7c853052ca
Zachary Miller <zachary.john.miller@gmail.com>
2018-10-19 11:00:19 -0600

    Rename 'Edit Allocation' to 'Edit Time'

--- client/src/pages/EpisodeShowPage/ChildrenSidebar.tsx
+++ client/src/pages/EpisodeShowPage/ChildrenSidebar.tsx
@@ -126,7 +126,7 @@ export class Child extends React.Component<any, any> {
                   this.setState({ showChildBudgetForm: true });
                 }}
               >
-                Edit Allocation
+                Edit Time
               </Button>
             )}
           <div style={{ float: "right" }}>
