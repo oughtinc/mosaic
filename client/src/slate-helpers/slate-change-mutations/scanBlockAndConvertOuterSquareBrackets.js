b23897200afef415fee71caa39dac47aa293308f
Zachary Miller <zachary.john.miller@gmail.com>
2018-10-24 16:53:39 -0600

    Handle links at the start of exported content

--- client/src/slate-helpers/slate-change-mutations/scanBlockAndConvertOuterSquareBrackets.js
+++ client/src/slate-helpers/slate-change-mutations/scanBlockAndConvertOuterSquareBrackets.js
@@ -1,3 +1,6 @@
+import { POINTER_EDGE_SPACE } from "../../lib/slate-pointers/exportedPointerSpacer";
+import { containingInlineAncestor } from "../slate-utils/containingInlineAncestor";
+
 export function scanBlockAndConvertOuterSquareBrackets ({ change, updateBlock, exportSelection, blockId }) {
   const value = change.value;
   const textNodes = value.document.getTexts();
@@ -36,6 +39,44 @@ function exportBracketedSelectionFromRange ({
   endKey,
   endOffset,
 }) {
+  const document = change.value.document;
+  const node = document.getNode(startKey)
+  const inlineParent = containingInlineAncestor(node, document);
+
+
+  // if the starting position of the selection is right at the start of a link,
+  // then Slate thinks of this position as "inside" the link, and the start of
+  // the newly created pointer will (inccorectly) be nested within the link,
+  // causing many problems
+
+  // we can fix this by first detecting that we are in such a problematic
+  // situation, we are in such a situation if the following
+  // isLinkAtStartOfExport is true
+
+  // we can then fix the problem by inserting a pointer edge space to the left
+  // of the link (taking care to avoid inserting it within the link itself)
+  // and then making sure we also export this pointer edge space, this
+  // guarantees that the link is wholly nested within the export
+
+  // the extra pointer edge space will disappear as soon as the export occurs
+  // thanks to the fact the occurrences of these characters are regulated
+  // and normalized by normalizeChange
+  const isLinkAtStartOfExport =
+    inlineParent
+    &&
+    inlineParent.type === "link"
+    &&
+    startOffset === 0;
+
+  let insertHereToProperlyHandleLinkAtStartOfExport;
+  if (isLinkAtStartOfExport) {
+    insertHereToProperlyHandleLinkAtStartOfExport = document.getPreviousText(node.key);
+    change
+      .moveToRangeOf(insertHereToProperlyHandleLinkAtStartOfExport)
+      .collapseToEnd()
+      .insertText(POINTER_EDGE_SPACE);
+  }
+
   // delete opening square bracket
   change.select({
     anchorKey: startKey,
@@ -59,12 +100,21 @@ function exportBracketedSelectionFromRange ({
   .deleteForward(1);
 
   // select everything that was within the square brackets
-  change.select({
-    anchorKey: startKey,
-    anchorOffset: startOffset,
-    focusKey: endKey,
-    focusOffset: startKey === endKey ? endOffset - 1 : endOffset,
-  });
+  if (isLinkAtStartOfExport) {
+    change.select({
+      anchorKey: insertHereToProperlyHandleLinkAtStartOfExport.key,
+      anchorOffset: insertHereToProperlyHandleLinkAtStartOfExport.text.length,
+      focusKey: endKey,
+      focusOffset: startKey === endKey ? endOffset - 1 : endOffset,
+    });
+  } else {
+    change.select({
+      anchorKey: startKey,
+      anchorOffset: startOffset,
+      focusKey: endKey,
+      focusOffset: startKey === endKey ? endOffset - 1 : endOffset,
+    });
+  }
 
   updateBlock({ id: blockId, value: change.value, pointerChanged: false });
   exportSelection(blockId);
