46db97f590ad552104a3694b1020270df842507e
Zachary Miller <zachary.john.miller@gmail.com>
2018-12-13 09:36:18 -0700

    Add colors warnings for input/output thresholds

--- client/src/pages/EpisodeShowPage/CharCountDisplays/index.tsx
+++ client/src/pages/EpisodeShowPage/CharCountDisplays/index.tsx
@@ -29,39 +29,46 @@ class CharCountDisplaysPresentational extends React.Component<any,  any> {
             }}
           >
             <Glyphicon glyph="copy" style={{ fontSize: "24px", marginRight: "5px" }}/>
-            input count
+            input
+            <div
+              style={{
+                color: this.props.inputCharCount < 300 ? "green" : (this.props.outputCharCount < 400 ? "yellow" : "red"),
+                fontSize: "24px",
+                margin: "3px 0 0 5px",
+                textAlign: "center",
+                textShadow: this.props.inputCharCount >= 300 && this.props.outputCharCount < 400 && "0 0 1px #333",
+              }}
+            >
+              {this.props.inputCharCount}
+            </div>
           </span>
-          <div
-            style={{
-              fontSize: "28px",
-              textAlign: "center",
-            }}
-          >
-            {this.props.inputCharCount}
-          </div>
         </span>
         <span>
-          <span
+        <span
+          style={{
+            alignItems: "center",
+            color: charCountDisplayHeaderFontColor,
+            display: "flex",
+            fontSize: charCountDisplayHeaderFontSize,
+            fontVariant: "small-caps",
+            fontWeight: 700,
+            justifyItems: "space-between",
+          }}
+        >
+          <Glyphicon glyph="paste" style={{ fontSize: "24px", marginRight: "5px" }}/>
+          output
+          <div
             style={{
-              alignItems: "center",
-              color: charCountDisplayHeaderFontColor,
-              display: "flex",
-              fontSize: charCountDisplayHeaderFontSize,
-              fontVariant: "small-caps",
-              fontWeight: 700,
-              justifyItems: "space-between",
-            }}
-          >
-            <Glyphicon glyph="paste" style={{ fontSize: "24px", marginRight: "5px" }}/>
-            output count
-          </span><div
-            style={{
-              fontSize: "28px",
+              color: this.props.outputCharCount < 300 ? "green" : (this.props.outputCharCount < 400 ? "yellow" : "red"),
+              fontSize: "24px",
+              margin: "3px 0 0 5px",
               textAlign: "center",
+              textShadow: this.props.outputCharCount >= 300 && this.props.outputCharCount < 400 && "0 0 1px #333",
             }}
           >
             {this.props.outputCharCount}
           </div>
+        </span>
       </span>
       </div>
     );
