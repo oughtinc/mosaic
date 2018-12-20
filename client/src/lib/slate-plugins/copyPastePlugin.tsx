41d25ca4c97b3a1c41e8ad23a2b1bdb955a4492c
Zachary Miller <zachary.john.miller@gmail.com>
2018-12-20 13:34:33 -0700

    Allow copy/paste of imports and links

--- client/src/lib/slate-plugins/copyPastePlugin.tsx
+++ client/src/lib/slate-plugins/copyPastePlugin.tsx
@@ -1,26 +1,85 @@
 import { getEventTransfer } from "slate-react";
 
+import * as uuidv1 from "uuid/v1";
+
 import { POINTER_EDGE_SPACE } from "../slate-pointers/exportedPointerSpacer";
 
-function CopyPastePlugin() {
+export function CopyPastePlugin() {
   return {
     onPaste: (event, change) => {
       const transfer = getEventTransfer(event);
-      let { text } = transfer;
+      const { fragment, text } = transfer;
 
-      text = text
-        .split(POINTER_EDGE_SPACE)
-        .join("");
+      if (fragment === null) {
+        change.insertText(processText(text));
+        return false;
+      }
 
-      text = text
-        .split("🔒")
-        .join("");
+      const processedInlineJSON = processDocumentJSONIntoInlineJSON(fragment.toJSON());
 
-      change.insertText(text);
-
+      change.insertInline(processedInlineJSON);
       return false;
     },
   };
 }
 
-export { CopyPastePlugin };
+function processDocumentJSONIntoInlineJSON(document) {
+  let i = 0;
+  let curBlock = document.nodes[i];
+  let nodes = [];
+
+  while(curBlock = document.nodes[i++]) {
+    nodes = nodes.concat(curBlock.nodes.map(processNode));
+  }
+
+  return {
+    object: "inline",
+    type: "pasted",
+    nodes,
+  };
+}
+
+export function processNode(node: any) {
+  if (node.type === "pointerImport") {
+    return {
+      ...node,
+      data: {
+        ...node.data,
+        internalReferenceId: uuidv1(), // generate new id so you can open/close this independently of the one it copied
+      }
+    };
+  } else if (node.type === "pointerExport") {
+    return {
+      object: "inline",
+      type: "pasted",
+      nodes: node.nodes.map(processNode),
+    }
+  } else if (node.object === "text") {
+    return {
+      object: "text",
+      leaves: node.leaves.map(processNode)
+    }
+  } else if (node.object === "leaf") {
+    return {
+      object: "leaf",
+      text: processText(node.text),
+    }
+  } else {
+    return {
+      ...node,
+      nodes: node.nodes.map(processNode),
+    }
+  }
+}
+
+function processText(text) {
+  text = text
+    .split(POINTER_EDGE_SPACE)
+    .join("");
+
+  text = text
+    .split("🔒")
+    .join("");
+
+  return text;
+}
