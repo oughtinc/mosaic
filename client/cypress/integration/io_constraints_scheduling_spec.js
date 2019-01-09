58e67d33f916602eea1df67fabfc945a3e909d78
Zachary Miller <zachary.john.miller@gmail.com>
2019-01-09 13:12:01 -0700

    Avoid square brackets in seed data content

--- client/cypress/integration/io_constraints_scheduling_spec.js
+++ client/cypress/integration/io_constraints_scheduling_spec.js
@@ -11,15 +11,15 @@ describe("Scheduling with only IO-constraints", function() {
     it("schedules typical user correctly", function() {
       cy.loopNTimes(10, {
         before: () => cy.visit("/next"),
-        during: () => cy.contains("[0 descendants]"),
+        during: () => cy.contains("(0 descendants)"),
         counts: [
           {
             min: 1,
-            testFn: $node => $node.text() === "A-1: A subquestion #1 [0 descendants]",
+            testFn: $node => $node.text() === "A-1: A subquestion #1 (0 descendants)",
           },
           {
             min: 1,
-            testFn: $node => $node.text() === "A-2-1: A-2 subquestion #1 [0 descendants]",
+            testFn: $node => $node.text() === "A-2-1: A-2 subquestion #1 (0 descendants)",
           },
         ],
       });
--- server/lib/testing/seedDbForTestingIOConstraintsScheduling.js
+++ server/lib/testing/seedDbForTestingIOConstraintsScheduling.js
@@ -17,24 +17,24 @@ export async function seedDbForTestingIOConstraintsScheduling() {
     isEligibleForAssignment: true,
     isPublic: true,
   }, {
-    questionValue: generateQuestionValueFromString("A: Root-level [3 descendants]"),
+    questionValue: generateQuestionValueFromString("A: Root-level (3 descendants)"),
   });
 
   const A1 = await A.createChild({
     creatorId: ID_OF_TYPICAL_USER_FOR_TESTING,
     totalBudget: 0,
-    question: generateQuestionValueFromString("A-1: A subquestion #1 [0 descendants]"),
+    question: generateQuestionValueFromString("A-1: A subquestion #1 (0 descendants)"),
   });
 
   const A2 = await A.createChild({
     creatorId: ID_OF_TYPICAL_USER_FOR_TESTING,
     totalBudget: 0,
-    question: generateQuestionValueFromString("A-2: A subquestion #2 [1 descendants]"),
+    question: generateQuestionValueFromString("A-2: A subquestion #2 (1 descendants)"),
   });
 
   const A2$1 = await A2.createChild({
     creatorId: ID_OF_TYPICAL_USER_FOR_TESTING,
     totalBudget: 0,
-    question: generateQuestionValueFromString("A-2-1: A-2 subquestion #1 [0 descendants]"),
+    question: generateQuestionValueFromString("A-2-1: A-2 subquestion #1 (0 descendants)"),
   });
 }
