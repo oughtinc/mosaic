901829e6c23ef3b7144b7741c4d366db9a571065
Zachary Miller <zachary.john.miller@gmail.com>
2019-01-11 11:15:10 -0700

    Add types to NumberOfStaleDescendantsCache

--- server/lib/scheduler/NumberOfStaleDescendantsCache.ts
+++ server/lib/scheduler/NumberOfStaleDescendantsCache.ts
@@ -1,6 +1,13 @@
 import * as _ from "lodash";
 import * as models from "../models";
 
+interface Workspace {
+  id: string;
+  isStale: boolean;
+  wasAnsweredByOracle: boolean;
+  getChildWorkspaces(): Workspace[];
+}
+
 class NumberOfStaleDescendantsCache {
   private static cache = new Map();
 
@@ -8,31 +15,38 @@ class NumberOfStaleDescendantsCache {
     this.cache = new Map();
   }
 
-  public static async getNumberOfStaleDescendants(workspace) {
-    const workspaceAlreadyCached = this.cache.has(workspace.id);
+  public static async getNumberOfStaleDescendants(workspace: Workspace): Promise<number> {
+    const workspaceAlreadyCached: boolean = this.cache.has(workspace.id);
 
     if (workspaceAlreadyCached) {
       return this.cache.get(workspace.id);
     }
 
+    const numberOfStaleDescendants: number = await this.calculateNumberOfStaleDescendants(workspace);
+
+    this.cache.set(workspace.id, numberOfStaleDescendants);
+
+    return numberOfStaleDescendants;
+  }
+
+  private static async calculateNumberOfStaleDescendants(workspace: Workspace): Promise<number> {
     const children = await workspace.getChildWorkspaces();
 
     if (children.length === 0) {
       this.cache.set(workspace.id, 0);
       return 0;
     } else {
-      let result = 0;
+      let result: number = 0;
       for (const child of children) {
         if (!child.wasAnsweredByOracle) {
           if (child.isStale) {
             result += 1;
           }
 
-          result += await NumberOfStaleDescendantsCache.getNumberOfStaleDescendants(child);  
+          result += await NumberOfStaleDescendantsCache.getNumberOfStaleDescendants(child);
         }
       }
 
-      this.cache.set(workspace.id, result);
       return result;
     }
   }
