173c15e94a7551027d04b91a8d1c57fbb79d664a
Zachary Miller <zachary.john.miller@gmail.com>
2019-01-24 11:08:05 -0700

    Modify input char limits

--- client/src/pages/EpisodeShowPage/CharCountDisplays/index.tsx
+++ client/src/pages/EpisodeShowPage/CharCountDisplays/index.tsx
@@ -32,14 +32,14 @@ class CharCountDisplaysPresentational extends React.Component<any,  any> {
             input
             <div
               style={{
-                color: this.props.inputCharCount < 300 ? "green" : (this.props.inputCharCount < 400 ? "yellow" : "red"),
+                color: this.props.inputCharCount < 200 ? "green" : (this.props.inputCharCount < 300 ? "yellow" : "red"),
                 fontSize: "24px",
                 margin: "3px 0 0 5px",
                 textAlign: "center",
-                textShadow: this.props.inputCharCount >= 300 && this.props.inputCharCount < 400 && "0 0 1px #333",
+                textShadow: this.props.inputCharCount >= 200 && this.props.inputCharCount < 300 && "0 0 1px #333",
               }}
             >
-              {400 - this.props.inputCharCount}
+              {300 - this.props.inputCharCount}
             </div>
           </span>
         </span>
