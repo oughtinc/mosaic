721faa93d6ad096eaa93ac069a960f4640f6988a
Zachary Miller <zachary.john.miller@gmail.com>
2019-02-07 10:14:47 -0700

    Allow front page format choice

--- client/src/pages/RootWorkspacePage/ListOfRootWorkspaces.tsx
+++ client/src/pages/RootWorkspacePage/ListOfRootWorkspaces.tsx
@@ -1,9 +1,17 @@
 import * as _ from "lodash";
 import * as React from "react";
+import { ToggleButton, ToggleButtonGroup } from "react-bootstrap";
 import { RootWorkspace } from "./RootWorkspace";
 
-const ListOfRootWorkspaces = ({ isLoading, workspaces }) => {
-  const experiments = _(workspaces)
+class ListOfRootWorkspaces extends React.Component<any, any> {
+  public state = {
+    displayFormat: "experiment",
+  };
+
+  public render() {
+    const { workspaces, isLoading } = this.props;
+    
+    const experiments = _(workspaces)
     .flatMap(w => w.tree.experiments)
     .uniqBy((e: any) => e.id)
     .sortBy((e: any) => -Date.parse(e.createdAt))
@@ -13,21 +21,50 @@ const ListOfRootWorkspaces = ({ isLoading, workspaces }) => {
     }))
     .value();
 
-  return (
-    <div>
-      {
-        isLoading
-        ?
-        "Loading..."
-        :
-          <div>
-            {
-              experiments.map(e => 
-                <div key={e.id}>
-                  <h1 style={{fontSize: "22px"}}>{e.name}</h1>
-                  {e.workspaces.map(w => 
+    return (
+      <div>
+        <ToggleButtonGroup
+          bsSize="xsmall"
+          type="radio" 
+          name="options" 
+          value={this.state.displayFormat}
+          onChange={this.handleDisplayFormatChange}
+        >
+          <ToggleButton value={"experiment"}> group by experiment</ToggleButton>
+          <ToggleButton value={"all"}>show all</ToggleButton>
+        </ToggleButtonGroup>
+        {
+          isLoading
+          ?
+          "Loading..."
+          :
+            <div>
+              {
+                this.state.displayFormat === "experiment"
+                &&
+                experiments.map(e => 
+                  <div key={e.id}>
+                    <h1 style={{fontSize: "22px"}}>{e.name}</h1>
+                    {e.workspaces.map(w => 
+                      <RootWorkspace
+                        key={`${e.id}${w.id}`}
+                        style={{
+                          margin: "5px 0",
+                        }}
+                        workspace={w}
+                      />
+                    )}
+                  </div>
+                )
+              }
+              {
+                this.state.displayFormat === "all"
+                &&
+                <div>
+                  <h1 style={{fontSize: "22px"}}>all workspaces</h1>
+                  {workspaces.map(w => 
                     <RootWorkspace
-                      key={`${e.id}${w.id}`}
+                      key={`${w.id}`}
                       style={{
                         margin: "5px 0",
                       }}
@@ -35,22 +72,16 @@ const ListOfRootWorkspaces = ({ isLoading, workspaces }) => {
                     />
                   )}
                 </div>
-              )
-            }
-            <h1 style={{fontSize: "22px"}}>all workspaces</h1>
-            {workspaces.map(w => 
-              <RootWorkspace
-                key={`${w.id}`}
-                style={{
-                  margin: "5px 0",
-                }}
-                workspace={w}
-              />
-            )}
-          </div>
-      }
-    </div>
-  );
-};
+              }
+            </div>
+        }
+      </div>
+    );
+  }
+
+  private handleDisplayFormatChange = value => {
+    this.setState({ displayFormat: value });
+  }
+}
 
 export { ListOfRootWorkspaces };
