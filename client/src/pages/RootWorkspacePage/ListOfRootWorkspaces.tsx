6d5150ab5c72d47c45d72c7df0c4f9475f814e37
Zachary Miller <zachary.john.miller@gmail.com>
2019-02-12 17:34:15 -0700

    Add experiment heading links back for admins

--- client/src/pages/RootWorkspacePage/ListOfRootWorkspaces.tsx
+++ client/src/pages/RootWorkspacePage/ListOfRootWorkspaces.tsx
@@ -3,6 +3,10 @@ import * as React from "react";
 import { ToggleButton, ToggleButtonGroup } from "react-bootstrap";
 import { RootWorkspace } from "./RootWorkspace";
 
+import { ExperimentLink } from "../../components/ExperimentLink";
+
+import { Auth } from "../../auth";
+
 class ListOfRootWorkspaces extends React.Component<any, any> {
   public state = {
     displayFormat: "experiment",
@@ -44,7 +48,15 @@ class ListOfRootWorkspaces extends React.Component<any, any> {
                 &&
                 experiments.map(e => 
                   <div key={e.id}>
-                    <h1 style={{fontSize: "22px"}}>{e.name}</h1>
+                    {
+                      Auth.isAdmin()
+                      ?
+                      <ExperimentLink experiment={e}>
+                        <h1 style={{fontSize: "22px"}}>{e.name}</h1>
+                      </ExperimentLink>
+                      :
+                      <h1 style={{fontSize: "22px"}}>{e.name}</h1>
+                    }
                     {e.workspaces.map(w => 
                       <RootWorkspace
                         key={`${e.id}${w.id}`}
