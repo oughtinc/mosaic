92af43f6b1f370398d6e6b590e46ac974f93e9b1
Zachary Miller <zachary.john.miller@gmail.com>
2019-03-11 12:53:31 -0600

    If tree expanded, show Collapse All btn

--- client/src/pages/WorkspaceSubtreePage/index.tsx
+++ client/src/pages/WorkspaceSubtreePage/index.tsx
@@ -4,21 +4,36 @@ import { Button } from "react-bootstrap";
 import { BlockHoverMenu } from "../../components/BlockHoverMenu";
 import { ContentContainer } from "../../components/ContentContainer";
 import { WorkspaceCard } from "../../components/WorkspaceCard";
+import { parse as parseQueryString } from "query-string";
 
 export class WorkspaceSubtreePage extends React.PureComponent<any, any> {
   public render() {
     const workspaceId = this.props.match.params.workspaceId;
     return (
       <ContentContainer>
-        <Button
-          onClick={() => {
-            const { origin, pathname } = window.location;
-            window.location.href = `${origin}${pathname}?expanded=true`;
-          }}
-          style={{ marginBottom: "20px" }}
-        >
-          Expand All
-        </Button>
+        {
+          parseQueryString(window.location.search).expanded === "true"
+          ?
+            <Button
+              onClick={() => {
+                const { origin, pathname } = window.location;
+                window.location.href = `${origin}${pathname}`;
+              }}
+              style={{ marginBottom: "20px" }}
+            >
+              Collapse All
+            </Button>
+          :
+            <Button
+              onClick={() => {
+                const { origin, pathname } = window.location;
+                window.location.href = `${origin}${pathname}?expanded=true`;
+              }}
+              style={{ marginBottom: "20px" }}
+            >
+              Expand All
+            </Button>
+        }
         <BlockHoverMenu>
           {workspaceId && (
             <WorkspaceCard
