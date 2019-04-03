7c1b6aac0518d70efe30b4c01b315a4edb0f879b
Zachary Miller <zachary.john.miller@gmail.com>
2019-04-03 14:35:42 -0600

    Fix spacing/indentation

--- server/lib/schema/helpers/getMessageForUser.ts
+++ server/lib/schema/helpers/getMessageForUser.ts
@@ -1,19 +1,19 @@
 export function getMessageForUser({isWorkspaceRootLevel, isThisFirstTimeWorkspaceHasBeenWorkedOn, typeOfUser}) {
-    if (typeOfUser === "TYPICAL") {
-        return null;
-      }
+  if (typeOfUser === "TYPICAL") {
+    return null;
+  }
 
-    if (!isThisFirstTimeWorkspaceHasBeenWorkedOn) {
-        if (typeOfUser === "HONEST") {
-            return (
+  if (!isThisFirstTimeWorkspaceHasBeenWorkedOn) {
+    if (typeOfUser === "HONEST") {
+      return (
 `**Instructions for returning honest oracle**\n
 &nbsp;\n
 1. Just click "Done!" under the response field`
-            );
-          }
+      );
+    }
 
-        if (typeOfUser === "MALICIOUS") {
-            return (
+    if (typeOfUser === "MALICIOUS") {
+      return (
 `**Instructions for returning malicious oracle**\n
 &nbsp;\n
 1. Check to see if the normal user has asked for clarification\n
@@ -22,32 +22,32 @@ export function getMessageForUser({isWorkspaceRootLevel, isThisFirstTimeWorkspac
    - Click "Done!" under the response field\n
 3. If the normal user has *not* asked for clarification:\n
    - Just click "Done!" under the response field\n`
-            );
-        }
+      );
     }
+  }
 
-    if (isWorkspaceRootLevel) {
-      return (
+  if (isWorkspaceRootLevel) {
+    return (
 `**Instructions for root level workspace**\n
 &nbsp;\n
 1. Formulate a question about the text referenced in the scratchpad, and write your question in the **New Question** field on the left\n
 2. Click "Submit"\n
 3. Click "Done!" under the response field`
-      );
-    }
+    );
+  }
 
-    if (typeOfUser === "HONEST") {
-      return (
+  if (typeOfUser === "HONEST") {
+    return (
 `**Instructions for honest oracle**\n
 &nbsp;\n
 1. Fill in a correct/helpful answer below\n
 2. Click "Submit" on the right\n
 3. Click "Done!" under the response field`
-      );
-    }
+    );
+  }
 
-    if (typeOfUser === "MALICIOUS") {
-      return (
+  if (typeOfUser === "MALICIOUS") {
+    return (
 `**Instructions for malicious oracle**\n
 &nbsp;\n
 1. Decide whether to challenge the honest oracle on this question and answer\n
@@ -58,8 +58,8 @@ export function getMessageForUser({isWorkspaceRootLevel, isThisFirstTimeWorkspac
 3. If you decide not to challenge:\n
    - Write "No challenge" in the response field\n
    - Click "Done!" under the response field`
-      );
-    }
-
-    return null;
+    );
   }
+
+  return null;
+}
