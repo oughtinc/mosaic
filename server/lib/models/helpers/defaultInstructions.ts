88f711ab734e659b105782f8d108de0880b2f0fe
Chris Northwood <chris@pling.org.uk>
2019-04-04 14:37:06 +0100

    Allow experiments to have instructions set on them

---
+++ server/lib/models/helpers/defaultInstructions.ts
@@ -0,0 +1,43 @@
+export const defaultRootInstructions = `**Instructions for root level workspace**
+
+1. Formulate a question about the text referenced in the scratchpad, and write your question in the **New Question** field on the left
+2. Click "Submit"
+3. Click "Done!" under the response field`;
+
+export const defaultHonestOracleInstructions = `**Instructions for honest oracle**
+
+1. Fill in a correct/helpful answer below
+2. Click "Submit" on the right
+3. Click "Done!" under the response field`;
+
+export const defaultMaliciousOracleInstructions = `**Instructions for malicious oracle**
+
+1. Decide whether to challenge the honest oracle on this question and answer
+2. If you decide to challenge:
+   - Fill in an incorrect/unhelpful but plausible alternative answer below
+   - Click "Submit" on the right
+   - Click "Done!" under the response field
+3. If you decide not to challenge:
+   - Click "Done!" under the response field`;
+
+export const defaultReturningRootInstructions = `**Instructions**
+
+1. Let the experiment administrator know that you see this message
+2. Please navigate to the main experiment page and rejoin the experiment`;
+
+export const defaultReturningHonestOracleInstructions = `**Instructions**
+
+1. Let the experiment administrator know that you see this message
+2. Please navigate to the main experiment page and rejoin the experiment`;
+
+export const defaultReturningMaliciousOracleInstructions = `**Instructions**
+
+1. Let the experiment administrator know that you see this message
+2. Please navigate to the main experiment page and rejoin the experiment`;
+
+export const defaultLazyPointerUnlockInstructions = `**Instructions for unlocking lazy pointer**
+
+1. Use the response field to describe the contents of the lazy pointer
+2. As an oracle, you can open the lazy pointer by clicking on it
+3. If you need more information than what you included in the lazy pointer, you can click the "Entire Tree »" button above to view the entire tree so far
+4. When you've finished, click "Done!" under the response field`;
