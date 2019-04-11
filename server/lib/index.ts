c5fd46949c3a2891a2140e4eb32b9009915b260d
Chris Northwood <chris@pling.org.uk>
2019-04-11 17:05:36 +0100

    update GraphQL/Apollo dependencies too

--- server/lib/index.ts
+++ server/lib/index.ts
@@ -4,9 +4,7 @@ import * as express from "express";
 import * as enforce from "express-sslify";
 
 import * as path from "path";
-import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
-import * as bodyParser from "body-parser";
-import * as cors from "cors";
+import { ApolloServer } from "apollo-server-express";
 
 import db from "./models";
 import { userFromAuthToken } from "./schema/auth/userFromAuthToken";
@@ -15,77 +13,68 @@ import { testingRoutes } from "./testing/routes";
 
 const GRAPHQL_PORT = process.env.PORT || 8080;
 
-const graphQLServer = express();
+const app = express();
+const server = new ApolloServer({
+  schema,
+  context: async ({ req }) => {
+    if (req.headers.authorization !== "null") {
+      const userInfo = await userFromAuthToken(req.headers.authorization);
+      let user = await db.models.User.findByPk(userInfo.user_id);
 
-graphQLServer.use(cors());
+      if (!user) {
+        user = await db.models.User.create({
+          id: userInfo.user_id,
+          givenName: userInfo.given_name,
+          familyName: userInfo.family_name,
+          email: userInfo.email,
+          gender: userInfo.gender,
+          pictureURL: userInfo.picture,
+        });
+      } else if (
+        (!user.givenName && userInfo.given_name)
+        ||
+        (!user.email && userInfo.email)
+      ) {
+        await user.update({
+          id: userInfo.user_id,
+          givenName: userInfo.given_name,
+          familyName: userInfo.family_name,
+          email: userInfo.email,
+          gender: userInfo.gender,
+          pictureURL: userInfo.picture,
+        });
+      }
+
+      return {
+        authorization: req.headers.authorization,
+        user,
+      };
+    } else {
+      return {
+        authorization: null,
+      };
+    }
+  },
+});
 
 if (!process.env.USING_DOCKER) {
-  graphQLServer.use(enforce.HTTPS({ trustProtoHeader: true }));
-  graphQLServer.use(
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(
     express.static(path.join(__dirname, "/../../client/build"))
   );
-  graphQLServer.get("*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname + "/../../client/build/index.html"));
   });
 }
 
-graphQLServer.use("/graphql", bodyParser.json(), async (req, res, next) => {
-  if (req.headers.authorization !== "null") {
-    const userInfo = await userFromAuthToken(req.headers.authorization);
-    let user = await db.models.User.findByPk(userInfo.user_id);
+server.applyMiddleware({ app });
 
-    if (!user) {
-      user = await db.models.User.create({
-        id: userInfo.user_id,
-        givenName: userInfo.given_name,
-        familyName: userInfo.family_name,
-        email: userInfo.email,
-        gender: userInfo.gender,
-        pictureURL: userInfo.picture,
-      });
-    } else if (
-      (!user.givenName && userInfo.given_name)
-      ||
-      (!user.email && userInfo.email)
-    ) {
-      await user.update({
-        id: userInfo.user_id,
-        givenName: userInfo.given_name,
-        familyName: userInfo.family_name,
-        email: userInfo.email,
-        gender: userInfo.gender,
-        pictureURL: userInfo.picture,
-      });
-    }
+app.use("/testing", testingRoutes);
 
-    return graphqlExpress({
-      schema,
-      context: {
-        authorization: req.headers.authorization,
-        user,
-      }
-    })(req, res, next);
-
-  } else {
-
-    return graphqlExpress({
-      schema,
-      context: {
-        authorization: null,
-      }
-    })(req, res, next);
-
-  }
-});
-
-graphQLServer.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
-
-graphQLServer.use("/testing", testingRoutes);
-
-graphQLServer.listen(GRAPHQL_PORT, () => {
+app.listen(GRAPHQL_PORT, () => {
   if (process.env.USING_DOCKER) {
     console.log(
-      `GraphiQL: http://localhost:${GRAPHQL_PORT}/graphiql \nReact: http://localhost:3000`
+      `GraphQL playground: http://localhost:${GRAPHQL_PORT}/graphql \nReact: http://localhost:3000`
     );
   }
   console.log(
