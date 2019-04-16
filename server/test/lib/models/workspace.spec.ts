9d24a759f4e4e22de112d4e9b800a86b444010fb
Chris Northwood <chris@pling.org.uk>
2019-04-16 13:29:15 +0100

    Make tests run

--- server/test/lib/models/workspace.spec.ts
+++ server/test/lib/models/workspace.spec.ts
@@ -1,22 +1,21 @@
 import * as chai from "chai";
 const { expect } = chai;
-import { truncateDb } from "../../testHelpers/sequelizeHelpers";
 import { rootWorkspaceQuestion } from "../../testHelpers/slateHelpers";
-import WorkspaceModel from "../../../lib/models/workspace";
-import * as models from "../../../lib/models";
+import Workspace from "../../../lib/models/workspace";
+import db from "../../../lib/models";
 
 describe("WorkspaceModel", () => {
   describe("subtreeWorkspaces", () => {
     context("a workspace with no child workspaces", () => {
-      beforeEach(() => truncateDb());
+      beforeEach(() => db.sync({ force: true }));
+
       it("should not return anything", async () => {
         const totalBudget = 1000;
         const creatorId = "testcreatorId";
         const question = rootWorkspaceQuestion("Mock workspace name");
-        const event = await models.Event.create();
-        const workspace = await models.Workspace.create(
+        const workspace = await Workspace.create(
           { totalBudget, creatorId },
-          { event, questionValue: JSON.parse(question) }
+          { questionValue: JSON.parse(question) }
         );
         const childWorkspaces = await workspace.getChildWorkspaces();
         expect(childWorkspaces).to.be.empty;
