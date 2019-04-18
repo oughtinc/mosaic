4d6b1e12ed41bb19fc4afb7c5b366e9fddab8288
Zachary Miller <zachary.john.miller@gmail.com>
2019-04-18 16:58:30 -0600

    Tweak compact tree layout

--- client/src/pages/CompactTreeView/CompactTreeRowLabel.tsx
+++ client/src/pages/CompactTreeView/CompactTreeRowLabel.tsx
@@ -9,7 +9,7 @@ export class CompactTreeRowLabel extends React.PureComponent<any, any> {
           fontWeight: 600,
           paddingRight: "10px",
           textAlign: "right",
-          minWidth: "35px",
+          minWidth: "50px",
           verticalAlign: "middle",
         }}
       >
